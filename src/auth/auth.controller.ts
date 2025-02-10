d6c53dd3e7bd1daff5e7cb07e7503cc661796489
PaperBackPear3 <fabioangeloni2001@gmail.com>
2025-02-10 19:34:57 +0100

    auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { UserDTO } from './dto/user';
 
 @Controller('auth')
 export class AuthController {
@@ -8,5 +9,20 @@ export class AuthController {
   @Get('')
   getHello(): string {
     return this.authService.getHello();
+  }
+
+  @Post('/login')
+  login(@Body() userDTO: UserDTO): string {
+    return this.authService.login(userDTO);
+  }
+
+  @Post('/register')
+  register(@Body() userDTO: UserDTO): string {
+    return this.authService.register(userDTO);
+  }
+
+  @Post('/logout')
+  logout(@Body() userDTO: UserDTO): string {
+    return this.authService.logout(userDTO.email);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { UserDTO } from './dto/user';
 
 @Injectable()
 export class AuthService {
   getHello(): string {
     return 'Hello Auth!';
+  }
+
+  login(userDTO: UserDTO): string {
+    // Implement login logic here
+    return `User ${userDTO.email} logged in  ${userDTO.password}`;
+  }
+
+  register(userDTO: UserDTO): string {
+    // Implement registration logic here
+    return `User ${userDTO.email} registered  ${userDTO.password}`;
+  }
+
+  logout(email: string): string {
+    // Implement logout logic here
+    return `User ${email} logged out`;
   }
 }
